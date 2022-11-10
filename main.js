b736144677f9a270e9c47861c32ffc0e0cf3a394
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2022-11-10 16:17:58 +0900

    기본 기능 완성

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const result = document.querySelector(".result span");
 const acBtn = document.querySelector(".btn--ac span");
 
 let decimalPoint = 0;
+let calculating = false;
+let calNum = 0;
+let kindOfCal;
 
 function onBtnClick(event) {
   const target = event.target;
@@ -10,13 +13,12 @@ function onBtnClick(event) {
   const resultText = result.textContent;
 
   if (targetText) {
-    result.scrollIntoView();
     switch (targetText) {
       case "AC":
         return;
       case "C":
         acBtnChange();
-        refreshResult(0);
+        refreshResult("0");
         decimalPoint = 0;
         break;
       case "%":
@@ -27,20 +29,20 @@ function onBtnClick(event) {
         break;
       case ".":
         if (!resultText.includes(".")) {
-          if (acBtn.textContent === "AC") {
-            acBtnChange();
-          }
+          if (acBtn.textContent === "AC") acBtnChange();
           refreshResult(resultText + ".");
         }
         break;
       default:
-        if (resultText.includes(".")) {
-          decimalPoint++;
+        if (calculating) {
+          calNum = resultText;
+          refreshResult(targetText);
+          calculating = !calculating;
+          return;
         }
+        if (resultText.includes(".")) decimalPoint++;
         if (resultText === "0") {
-          if (targetText === "0") {
-            return;
-          }
+          if (targetText === "0") return;
           acBtnChange();
           refreshResult(targetText);
         } else {
@@ -48,10 +50,72 @@ function onBtnClick(event) {
         }
         break;
     }
+  } else {
+    const id = target.id;
+    if (id === "sign") {
+      switch (resultText) {
+        case "0":
+          refreshResult("-0");
+          break;
+        case "-0":
+          refreshResult("0");
+          break;
+        default:
+          refreshResult(-resultText);
+      }
+    } else if (id === "equal") {
+      const nowResult = result.textContent;
+
+      switch (kindOfCal) {
+        case "div":
+          changeColor("divide");
+          refreshResult(eval(calNum + "/" + nowResult));
+          break;
+        case "multi":
+          changeColor("multi");
+          refreshResult(eval(calNum + "*" + nowResult));
+          break;
+        case "minus":
+          changeColor("minus");
+          refreshResult(eval(calNum + "-" + nowResult));
+          break;
+        case "plus":
+          changeColor("plus");
+          refreshResult(eval(calNum + "+" + nowResult));
+          break;
+      }
+      calNum = 0;
+      kindOfCal = undefined;
+    } else {
+      const btnNode = target.parentElement;
+
+      calculating = !calculating;
+      btnNode.style.color = "orange";
+      btnNode.style.backgroundColor = "white";
+
+      switch (id) {
+        case "div":
+          kindOfCal = "div";
+          break;
+        case "multi":
+          kindOfCal = "multi";
+          break;
+        case "minus":
+          kindOfCal = "minus";
+          break;
+        case "plus":
+          kindOfCal = "plus";
+          break;
+      }
+    }
   }
 }
 
-function onSpanClick() {}
+function changeColor(value) {
+  const btn = document.querySelector(`.btn--${value}`);
+  btn.style.color = "white";
+  btn.style.backgroundColor = "orange";
+}
 
 function acBtnChange() {
   if (acBtn.textContent === "AC") {
